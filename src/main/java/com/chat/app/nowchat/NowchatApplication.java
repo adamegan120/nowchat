a25cc99c5c70ae7cff72357758ae2ef18057983d
adamegan120 <adam14egan@gmail.com>
2024-03-02 17:37:56 +0000

    init commit

---
+++ src/main/java/com/chat/app/nowchat/NowchatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.app.nowchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowchatApplication.class, args);
+	}
+
+}
